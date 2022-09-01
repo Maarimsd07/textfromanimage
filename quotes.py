365cbd031a9532a3fb98ca7a24471bd04956cd0e
Maarimsd07 <111353650+Maarimsd07@users.noreply.github.com>
2022-09-01 17:47:41 +0530

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -2,7 +2,7 @@ import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 from PIL import Image
 
-img = Image.open('textquotes.png')
-text = tess.image_to_string(img)
+mahesh = Image.open('textquotes.png')
+text = tess.image_to_string(mahesh)
 
 print(text)
